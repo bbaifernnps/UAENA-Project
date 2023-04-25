34aec322c180183361c55bac433a3a87d751609c
bbaifernnps <bbaifernnps@gmail.com>
2023-04-25 23:55:31 +0100

    comments added

--- assets/js/photogallery.js
+++ assets/js/photogallery.js
@@ -1,10 +1,12 @@
 document.querySelectorAll('.image-container img').forEach(image =>{
+    //Shoe popup image and set source to clicked image
     image.onclick = () => {
         document.querySelector('.popup-image').style.display = 'block';
         document.querySelector('.popup-image img').src = image.getAttribute('src');
     }
 });
 
+//Onclick function to close button 
 document.querySelector('.popup-image span').onclick = () => {
     document.querySelector('.popup-image').style.display = 'none';
 }
