ca7bba94fd62419b759878b75058820709a277ba
Tawatchai Suktrakul <Tawatchai.Suktrakul@gmail.com>
2023-04-27 15:11:43 +0700

    Biography remove white rectangle background and add function to button disco.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 function redirectToMenuPage() {
-    window.location.href="/menu/menu.html";  
-  }
+  window.location.href="/menu/menu.html";  
+}
+function redirectToPhotoGalleryPage() {
+window.location.href="/photogallery/photogallery.html";  
+}
+function redirectToMusicVideoPage() {
+window.location.href="/musicvideo/musicvideo.html";  
+}
+function redirectToMoviesPage() {
+window.location.href="/movies/movie.html";  
+}
+function redirectToDiscographyPage() {
+window.location.href="/discography/discography.html";  
+}
+function redirectToBiographyPage() {
+window.location.href="/biography/biography.html";  
+}
